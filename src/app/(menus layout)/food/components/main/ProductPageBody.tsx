b60745bc2fac88b001e2957fcb642bc4f162f817
Vicky Maulana <vickymaulana0710@gmail.com>
2025-05-15 01:23:29 +0700

    fix: minor fixing user interface

--- src/app/(menus layout)/food/components/main/ProductPageBody.tsx
+++ src/app/(menus layout)/food/components/main/ProductPageBody.tsx
@@ -7,6 +7,7 @@ import Error from "@/components/shared/http-response/Error";
 import Null from "@/components/shared/http-response/Null";
 import Image from "next/image";
 import { rupiah } from "@/utils/currency";
+import Link from "next/link";
 
 type TProductPageBodyProps = {
   action: {
@@ -30,7 +31,10 @@ const ProductPageBody: React.FC<TProductPageBodyProps> = () => {
   if (isError) return <Error />;
 
   return (
-    <>
+    <div className="bg-white ">
+      <div className="pt-6 ps-6">
+        <Link href='/food/create' className="bg-blue-500 text-white p-2 cursor-pointer">+ Tambah Menu</Link>
+      </div>
       {isLoading && (
         <div className="flex justify-center items-center py-8">
           <div className="w-8 h-8 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
@@ -38,42 +42,42 @@ const ProductPageBody: React.FC<TProductPageBodyProps> = () => {
       )}
 
       {!isLoading && !isError && products && (
-        <div className="overflow-x-auto">
+        <div className="overflow-x-auto p-6">
           {products.data.length > 0 ? (
             <>
               <table className="min-w-full text-left text-sm border-collapse">
-                <thead className="bg-white">
+                <thead className="bg-gray-200">
                   <tr>
-                    <th className="min-w-[150px] p-2 font-medium text-gray-700">
+                    <th className="min-w-[150px] p-4 font-bold text-gray-700">
                       #
                     </th>
-                    <th className="min-w-[200px] p-2 font-medium text-gray-700">
+                    <th className="min-w-[200px] p-4 font-bold text-gray-700">
                       Nama
                     </th>
-                    <th className="min-w-[150px] p-2 font-medium text-gray-700">
+                    <th className="min-w-[150px] p-4 font-bold text-gray-700">
                       Foto
                     </th>
-                    <th className="p-2 font-medium text-gray-700">Harga</th>
+                    <th className="p-4 font-bold text-gray-700">Harga</th>
                   </tr>
                 </thead>
                 <tbody>
                   {products.data.map((value, index) => (
                     <React.Fragment key={value.id}>
-                      <tr className="border-b last:border-none">
-                        <td className="p-2">{index + 1}</td>
-                        <td className="p-2">{value.name}</td>
-                        <td className="p-2">
+                      <tr className="border-b last:border-none odd:bg-white even:bg-gray-200">
+                        <td className="p-4">{index + 1}</td>
+                        <td className="p-4">{value.name}</td>
+                        <td className="p-4">
                           <div className="flex items-center">
                             <Image
                               src={value.image}
                               alt={value.name}
-                              className="w-10 h-10 object-cover rounded"
-                              width={40}
-                              height={40}
+                              className="w-40 h-40 object-cover rounded"
+                              width={100}
+                              height={100}
                             />
                           </div>
                         </td>
-                        <td className="p-2">{rupiah(value.price)}</td>
+                        <td className="p-4">{rupiah(value.price)}</td>
                       </tr>
                     </React.Fragment>
                   ))}
@@ -162,7 +166,7 @@ const ProductPageBody: React.FC<TProductPageBodyProps> = () => {
           )}
         </div>
       )}
-    </>
+    </div>
   );
 };
 
--- src/app/(menus layout)/food/components/main/ProductPageHeader.tsx
+++ src/app/(menus layout)/food/components/main/ProductPageHeader.tsx
@@ -8,7 +8,7 @@ const ProductPageHeader: React.FC<TProductPageHeaderProps> = ({  }) => {
 
   return (
     <header className="pt-10">
-      <div className="d-flex align-items-center text-gray-400">
+      <div className="d-flex align-items-center text-gray-500 my-4">
         <h1 className="contactapp-title link-dark mb-0">Tambahkan menu makanan yang ada di resto</h1>
       </div>
     </header>
